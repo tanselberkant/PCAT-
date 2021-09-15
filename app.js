3c93fe1e6a14d1e98c81c772cb66374a07e274b0
tanselberkant <tanselberkant@gmail.com>
2021-09-16 02:30:27 +0300

    Ejs Workspace Configuration

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const path = require('path');
+const ejs = require('ejs');
+
 const app = express();
 const port = 3000;
 
-// Middlewares
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs'); // We saying to express that we are gonna use ejs for template engine
+
+// MIDDLEWARES
 app.use(express.static('public')); // Statik dosyalarımız için publik klasörü olusturmak
 
+// ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 app.listen(port, () => {
