931d28ba64525b88902d9c4b42d0c53a1c199011
tanselberkant <tanselberkant@gmail.com>
2021-09-15 22:23:31 +0300

    Nodemon & Express

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo Name',
+    description: 'Photo Description',
+  };
+
+  res.send(photo);
+});
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda baslatildi..`);
+});
