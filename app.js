767ea416bd460eafcb848c051ade7b68f5589602
tanselberkant <tanselberkant@gmail.com>
2021-09-19 19:37:18 +0300

    Crud-1 Update a photo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fileUpload = require('express-fileupload');
+const methodOverride = require('method-override');
 const path = require('path');
 const ejs = require('ejs');
 const fs = require('fs');
@@ -23,6 +24,7 @@ app.use(express.static('public')); // Statik dosyalarımız için publik klasör
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(methodOverride('_method'));
 
 // ROUTES
 app.get('/', async (req, res) => {
@@ -70,6 +72,23 @@ app.post('/photos', async (req, res) => {
     });
     res.redirect('/');
   });
+});
+
+// Update photo
+app.get('/photos/edit/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render('edit', {
+    photo,
+  });
+});
+
+app.put('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+  
+  res.redirect(`/photos/${req.params.id}`);
 });
 
 const port = 3000;
